51dbbde118ff07d21365415c13a7cebe155f6551
Memphis DiAngelis <earthtomemphis@gmail.com>
2020-11-21 17:54:37 -0700

    deliver or takeout choice -Success

--- takeout/admin.py
+++ takeout/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Order
+from .models import Order, Menu
 
 # Register your models here.
+admin.site.register(Menu)
 admin.site.register(Order)
--- takeout/models.py
+++ takeout/models.py
@@ -2,21 +2,39 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Menu(models.Model):
+    dish_name = models.CharField(max_length=255)
+    dish_image = models.ImageField()
+    dish_price = models.FloatField()
+    dish_calories = models.IntegerField()
+
+    def __str__(self):
+        return str(self.dish)
+
 class Order(models.Model):
-    first_name = models.CharField(max_length=255, null=True)
-    last_name = models.CharField(max_length=255, null=True)
-    email = models.EmailField(max_length=255, null=True)
-    address_line1 = models.CharField(max_length=255, null=True)
-    address_line2 = models.CharField(max_length=255, null=True)
-    city = models.CharField(max_length=255, null=True)
+    first_name = models.CharField(max_length=30, null=True)
+    last_name = models.CharField(max_length=30, null=True)
+    email = models.EmailField(max_length=30, null=True)
+    address_line1 = models.CharField(max_length=30, null=True)
+    address_line2 = models.CharField(max_length=30, null=True)
+    city = models.CharField(max_length=30, null=True)
     zipcode = models.IntegerField(null=True)
-    menu_item1 = models.CharField(max_length=255, null=True)
-    menu_item2 = models.CharField(max_length=255, null=True)
-    menu_item3 = models.CharField(max_length=255, null=True)
-    delivery_pickup = models.CharField(max_length=255, null=True)
+    menu_item1 = models.CharField(max_length=30, null=True)
+    menu_item2 = models.CharField(max_length=30, null=True)
+    menu_item3 = models.CharField(max_length=30, null=True)
+    delivery = 'Delivery'
+    takeout = 'Takeout'
+    DELIVERY_PICKUP_CHOICE = [
+        (delivery, 'Delivery'),
+        (takeout, 'Takeout'),
+    ]
+    delivery_pickup = models.CharField(max_length=8, choices=DELIVERY_PICKUP_CHOICE, default=takeout)
     payment_method = models.CharField(max_length=255, null=True)
     password1 = models.CharField(max_length=255, null=True)
     password2 = models.CharField(max_length=255, null=True)
 
     def __str__(self):
         return str(self.user)
+
+    def delivery_pickup_choice(self):
+        return self.delivery_pickup in {self.delivery, self.pickup} 
